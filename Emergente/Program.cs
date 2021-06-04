2ab84b2919ebd3b5813c8aeb676a50f3d6316947
FabioVasquez69 <fabio.vaz93@hotmail.com>
2021-06-04 09:02:36 -0500

    modalEmergente

---
+++ Emergente/Program.cs
@@ -0,0 +1,34 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+using FluentAssertions;
+using System;
+
+namespace Emergente
+{
+    public class UnitTest1
+    {
+        static void Main(string[] args)
+        {
+                        
+            ChromeOptions full = new ChromeOptions();
+            full.AddArgument("start-maximized");
+            ChromeDriver driver = new ChromeDriver(full);
+
+            driver.Url = "https://demoqa.com/alerts";
+            var buttonClickMe = driver.FindElement(By.XPath("//button[text()= 'Click me' and @id='confirmButton']"));
+            buttonClickMe.Click();
+            var alert = driver.SwitchTo().Alert();
+            Console.WriteLine("El texto es " +alert.Text);
+            
+            alert.Accept();
+
+            var msnConfirmation = driver.FindElement(By.XPath("//span[@id='confirmResult']")).Text;
+            msnConfirmation.Should().Equals("You selected Ok");
+            Thread.Sleep(9000);
+            buttonClickMe.Click();
+
+            driver.Quit();
+        }
+    }
+}
